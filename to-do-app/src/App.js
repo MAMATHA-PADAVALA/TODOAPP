a21b9d2efbed898592489a1d320960d5cdb56e3d
MAMATHA-PADAVALA <mamathapadavala146@gmail.com>
2023-12-23 12:48:16 +0530

    second commit Empty Tasks

--- to-do-app/src/App.js
+++ to-do-app/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { useState } from 'react';
+import './App.css';
+import Header from './Header/Header';
+import Task from './Task/Task';
+
+export default function App(){
+
+
+  const [todoList,setToDoList]=useState(["Read SpringBoot", 
+  "Complete assignments", 
+  "Prepare breakfast", 
+  "Sleep for 2 hours", 
+  "Take a shower"]);
+
+  const removeAllTasks = () => {
+    setToDoList([]);
+  };
+
+      return(
+        <div className="App">
+          <Header/>
+          <hr/>
+
+          <div>
+        {todoList.map((todo) => (
+          <>
+            <div key={todo}></div>
+            <Task task={todo}/>
+          </>
+        ))}
+      </div>
+
+      <div>
+        <div>
+      {todoList.length === 0 ? (
+          <i>Nothing to do buddy. Sleep!</i>
+        ):""
+        }
+        </div>
+        
+          <button onClick={() => removeAllTasks()}>
+            Empty
+          </button>
+          
+          
+      </div>
+          
+        </div>
+      );
+           
 }
 
-export default App;
+
